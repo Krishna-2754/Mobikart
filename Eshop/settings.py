0a7136dd1443a3535c1668592ba2d2fc59ce76de
Krishna-2754 <74001038+Krishna-2754@users.noreply.github.com>
2022-01-11 18:44:02 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -77,12 +77,14 @@ WSGI_APPLICATION = 'Eshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sdp',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sdp',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
